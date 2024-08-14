f3ffcf951f72c75c7ed372fecfecfe9994e3b2ee
Emmanuel Pius <emmycode4495@gmail.com>
2024-08-14 17:24:20 +0100

    Added animations to the call screen

--- app/(tabs)/calls/_layout.tsx
+++ app/(tabs)/calls/_layout.tsx
@@ -13,7 +13,9 @@ const Layout = () => {
           title: "Calls",
           headerLargeTitle:true,
           headerShadowVisible:false,
-          headerStyle:{backgroundColor:Colors.background},
+          headerBlurEffect:'regular',
+          headerTransparent:true,
+          // headerStyle:{backgroundColor:Colors.background},
           headerSearchBarOptions:{
             placeholder:'Search'
           },
--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -1,25 +1,58 @@
 import { View, Text, TouchableOpacity, ScrollView, FlatList, StyleSheet, Image } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { Stack } from 'expo-router'
 import Colors from '@/constants/Colors'
 import calls from '@/assets/data/calls.json'
 import { defaultStyles } from '@/constants/Styles'
 import { Ionicons } from '@expo/vector-icons'
 import { format } from 'date-fns'
+import { SegmentedControl } from '@/components/SegmentedControl'
+import Animated, { CurvedTransition, FadeInUp, FadeOutUp, useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import SwipeableRow from '@/components/SwipeableRow'
+import * as Haptics from 'expo-haptics'
+
+const transition = CurvedTransition.delay(100);
+
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity)
 
 const Page = () => {
   const [isEditing, setIsEditing] = useState(false)
   const [items, setItems] = useState(calls);
+  const [selectedOption, setSelectedOption] = useState("All")
+  const editing = useSharedValue(-30)
 
   const onEdit = () =>{
     let editingNew = !isEditing
+    editing.value = editingNew ? 0 : -30
     setIsEditing(editingNew)
   }
+
+  useEffect(()=>{
+    if(selectedOption === 'All'){
+      setItems(calls)
+    }else{
+      setItems(calls.filter((item)=>item.missed))
+    }
+  }, [selectedOption])
+
+  const removeCall = (item: any) =>{
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+    setItems(items.filter((i)=> i.id !== item.id))
+  }
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform:[{translateX: editing.value}]
+  }))
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors.background }}>
       <Stack.Screen
         options={{
+          headerTitle: () =>(
+            <SegmentedControl options={['All', 'Missed']} selectedOption={selectedOption}
+            onOptionPress={setSelectedOption}
+            />
+          ),
           headerLeft: () => (
             <TouchableOpacity onPress={onEdit}>
               <Text style={{ color: Colors.primary, fontSize: 18 }}>
@@ -30,16 +63,23 @@ const Page = () => {
         }}
       />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View style={defaultStyles.block}>
-          <FlatList
+        <Animated.View style={defaultStyles.block} layout={transition}>
+          <Animated.FlatList
+          skipEnteringExitingAnimations
             data={items}
             scrollEnabled={false}
             contentContainerStyle={{paddingBottom:40}}
+            itemLayoutAnimation={transition}
             keyExtractor={(item) => item.id.toString()}
             ItemSeparatorComponent={() => (
               <View style={defaultStyles.separator} />
             )}
-            renderItem={({ item }) => (
+            renderItem={({ item, index}) => (
+              <SwipeableRow onDelete={() => removeCall(item)}>
+              <Animated.View entering={FadeInUp.delay(index * 20)} exiting={FadeOutUp}>
+                <AnimatedTouchableOpacity onPress={()=>removeCall(item)}
+                style={[animatedRowStyles]}
+                ></AnimatedTouchableOpacity>
               <View style={[defaultStyles.item]}>
                 <Image source={{ uri: item.img }} style={styles.avatar} />
                 <View style={{ flex: 1, gap: 2 }}>
@@ -78,9 +118,11 @@ const Page = () => {
                       />
                     </View>
               </View>
+              </Animated.View>
+              </SwipeableRow>
             )}
           />
-        </View>
+        </Animated.View>
       </ScrollView>
     </View>
   );
